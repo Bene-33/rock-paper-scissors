3cc1a8467bbf2626089a6b1c46bd6c195898afa2
Tropfig <82678066+Tropfig@users.noreply.github.com>
2023-09-30 15:25:15 +0200

    delet to Lower Case Function for player selection switcht to simple let declaration add first playround function, function still bugged

--- gamescript.js
+++ gamescript.js
@@ -1,15 +1,33 @@
-const selection = ["rock","paper","scissors"] 
+const selection = ["rock","paper","scissors"];
+const playerChoice = "RoCk"
+const cChoice = computerChoice(selection);
+let pChoice = playerChoice.toLowerCase();
 
-function computerChoice(selection) {
-    compChoice = Math.floor(Math.random() * selection.length);
-    return selection[compChoice];
+//this function pseudo random select a computer result
+function computerChoice(string) {
+    compChoice = Math.floor(Math.random() * string.length);
+    return string[compChoice];
 }
 
-console.log(computerChoice(selection))
-
-function playersChoice(word){
-    return word.toLowerCase()
-    
+//game function to select who beets who
+function playRound(cChoice, pChoice){
+    if (cChoice === pChoice){
+        return "draw"
+    }
+        else if (cChoice === "scissors" && pChoice === "rock"){
+            return "you won, rock beats scissors";
+        }
+        else if (cChoice === "rock" && pChoice === "paper"){
+            return "you won, paper beats rock";
+        }
+        else if (cChoice === "paper" && pChoice === "scissors"){
+            return "you won, scissors beats paper";
+        }
+    else {
+        return "computer won"
+    }
 }
 
-console.log(playersChoice("rock"))
+console.log(cChoice)
+console.log(pChoice)
+console.log(playRound(cChoice, pChoice))
