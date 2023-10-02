9e4d758881e03d5847f81b6c5eff783520f54a28
Tropfig <82678066+Tropfig@users.noreply.github.com>
2023-10-02 13:24:01 +0200

    rename result of computerChoice and playerChoice

--- gamescript.js
+++ gamescript.js
@@ -1,7 +1,9 @@
 const selection = ["rock","paper","scissors"];
-const playerChoice = "rock"
-const cChoice = computerChoice(selection);
-let pChoice = playerChoice.toLowerCase();
+const playerChoice = "rock";
+const computerSelection = computerChoice(selection);
+let playerSelection = playerChoice.toLowerCase();
+let playerScore = 0;
+let computerScore = 0;
 
 //this function pseudo random select a computer result
 function computerChoice(string) {
@@ -10,30 +12,41 @@ function computerChoice(string) {
 }
 
 //game function to select who beets who
-function playRound(cChoice, pChoice){
-    if (cChoice === pChoice){
+function playRound(computerSelection, playerSelection){
+    if (computerSelection === playerSelection){
         return "draw"
     }
-        else if (cChoice === "scissors" && pChoice === "rock"){
+        else if (computerSelection === "scissors" && playerSelection === "rock"){
             return "you won, rock beats scissors";
         }
-        else if (cChoice === "rock" && pChoice === "paper"){
+        else if (computerSelection === "rock" && playerSelection === "paper"){
             return "you won, paper beats rock";
         }
-        else if (cChoice === "paper" && pChoice === "scissors"){
+        else if (computerSelection === "paper" && playerSelection === "scissors"){
             return "you won, scissors beats paper";
         }
-        else if (cChoice === "rock" && pChoice === "scissors"){
+        else if (computerSelection === "rock" && playerSelection === "scissors"){
             return "you lost, rock beats scissors";
         }
-        else if (cChoice === "paper" && pChoice === "rock"){
+        else if (computerSelection === "paper" && playerSelection === "rock"){
             return "you lost, paper beats rock";
         }
-        else if (cChoice === "scissors" && pChoice === "paper"){
+        else if (computerSelection === "scissors" && playerSelection === "paper"){
             return "you lost, scissors beats paper";
         }
         else "error"
 }
 
-console.log(cChoice, pChoice)
-console.log(playRound(cChoice, pChoice))
+function game(rounds){
+    for(let rounds = 0; rounds < 5; rounds++)
+        //prompt(`Choose between ${selection}`)
+        if(playRound(computerSelection, playerSelection).substring(0,7)==="you won"){
+            return playerScore++
+        }
+        else if(playRound(computerSelection,playerSelection).substring(0,8)=== "you lost"){
+            return computerScore++
+        }
+        else ""
+}
+console.log(computerSelection, playerSelection)
+console.log(playRound())
