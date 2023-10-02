c2390c1209b8e22d2733bf960b596b8e43305dd5
Tropfig <82678066+Tropfig@users.noreply.github.com>
2023-10-02 22:20:38 +0200

    formatting

--- gamescript.js
+++ gamescript.js
@@ -1,10 +1,9 @@
 const rounds = 5;
 
-
 function game(){
 
-    let playerScore = 3;
-    let computerScore = 1;
+    let playerScore = 0;
+    let computerScore = 0;
 
     // play 'rounds' of the game
     for(let i = 0; i< rounds; i++){
@@ -23,54 +22,44 @@ function game(){
         function playRound(){
             if (computerSelection.substr(1) === playerSelection){
                 return{
-                    message: "draw"};
-                }
-                else if 
-                (computerSelection.substr(1) === "scissors" && playerSelection === "rock"){
-                    let result = 0;
-                    playResult = result++;
+                    message: "draw",
+                };
+            }
+            else if (computerSelection.substr(1) === "scissors" && playerSelection === "rock"){
                     return{
                         message: "you won, rock beats scissors",
-                        playerScore: playResult ,
                     };
-
-                }
-                else if (computerSelection.substr(1) === "rock" && playerSelection === "paper"){
-                    return{
-                        message: "you won, paper beats rock",
-                    };
-
-                }
-                else if (computerSelection.substr(1) === "paper" && playerSelection === "scissors"){
-                    return{
-                        message: "you won, scissors beats paper",
-                    };
-
-                }
-                else if (computerSelection.substr(1) === "rock" && playerSelection === "scissors"){
-                    return{
-                        message: "you lost, rock beats scissors"
-                    };
-
-                }
-                else if (computerSelection.substr(1) === "paper" && playerSelection === "rock"){
-                    return{
-                        message: "you lost, paper beats rock"
-                    };
-
-                }
-                else if (computerSelection.substr(1) === "scissors" && playerSelection === "paper"){
-                    return{
-                        messsage: "you lost, scissors beats paper"
-                    };
-
-                }
-                else {
-                    return{
-                        message: "something went wrong",
-                    };
-                }
-
+            }
+            else if (computerSelection.substr(1) === "rock" && playerSelection === "paper"){
+                return{
+                    message: "you won, paper beats rock",
+                };
+            }
+            else if (computerSelection.substr(1) === "paper" && playerSelection === "scissors"){
+                return{
+                    message: "you won, scissors beats paper",
+                };
+            }
+            else if (computerSelection.substr(1) === "rock" && playerSelection === "scissors"){
+                return{
+                    message: "you lost, rock beats scissors"
+                };
+            }
+            else if (computerSelection.substr(1) === "paper" && playerSelection === "rock"){
+                return{
+                    message: "you lost, paper beats rock"
+                };
+            }
+            else if (computerSelection.substr(1) === "scissors" && playerSelection === "paper"){
+                return{
+                    messsage: "you lost, scissors beats paper"
+                };
+            }
+            else {
+                return{
+                    message: "something went wrong",
+                };
+            }
         }
         const getRoundResult = playRound();
         let roundResult = getRoundResult.message;
@@ -88,15 +77,18 @@ function game(){
         if(i=rounds-1){
             if(playerScore > computerScore){
                 return{
-                    message: "you smashed the computer and won"};
+                    message: "you smashed the computer and won",
+                };
             }
             else if(computerScore > playerScore){
                 return{
-                    message: "the computer smashed you and won"};
+                    message: "the computer smashed you and won",
+                };
             }
             else if(computerScore === playerScore){
                 return{
-                    message: "its a draw and noone won"};
+                    message: "its a draw and noone won",
+                };
             }
         }
     }
@@ -104,6 +96,5 @@ function game(){
     let finalResult = getAnnounceResult.message;
     console.log(finalResult)
 } 
-
 
 game()
