5bfbee24e6643192c67e55052597eda8d387c23d
Tropfig <82678066+Tropfig@users.noreply.github.com>
2023-10-02 21:38:01 +0200

    changed announceResult now use global variable directily

--- gamescript.js
+++ gamescript.js
@@ -10,10 +10,8 @@ function game(){
     for(let i = 0; i< rounds; i++){
         const selection = [" rock"," paper"," scissors"]; // space to use it with the player prompt 
         let playerChoice = "rock"; //prompt(`Choose between${selection} and beat the computer`,"")
-        let computerSelection = " scissors"//computerChoice(selection);
+        let computerSelection = " scissors" //computerChoice(selection);
         let playerSelection = playerChoice.toLowerCase();
-        let score;
-
 
         //this function pseudo random select a computer result
         function computerChoice(string) {
@@ -22,60 +20,76 @@ function game(){
             }
 
         //game function to select who beets who
-        function playRound(computerSelection, playerSelection){
+        function playRound(){
             if (computerSelection.substr(1) === playerSelection){
                 return "draw"
                 }
                 else if 
                 (computerSelection.substr(1) === "scissors" && playerSelection === "rock"){
-                    return "you won, rock beats scissors";
+                    let result = 0;
+                    playerScore = result++;
+                    return{
+                        message: "you won, rock beats scissors",
+                        score: playerScore,
+                    };
 
                 }
                 else if (computerSelection.substr(1) === "rock" && playerSelection === "paper"){
-                    return "you won, paper beats rock";
+                    return{
+                        message: "you won, paper beats rock",
+                    };
 
                 }
                 else if (computerSelection.substr(1) === "paper" && playerSelection === "scissors"){
-                    return "you won, scissors beats paper";
+                    return{
+                        message: "you won, scissors beats paper",
+                    };
 
                 }
                 else if (computerSelection.substr(1) === "rock" && playerSelection === "scissors"){
-                    return "you lost, rock beats scissors";
+                    return{
+                        message: "you lost, rock beats scissors"
+                    };
 
                 }
                 else if (computerSelection.substr(1) === "paper" && playerSelection === "rock"){
-                    return "you lost, paper beats rock";
+                    return{
+                        message: "you lost, paper beats rock"
+                    };
 
                 }
                 else if (computerSelection.substr(1) === "scissors" && playerSelection === "paper"){
-                    return "you lost, scissors beats paper";
+                    return{
+                        messsage: "you lost, scissors beats paper"
+                    };
 
                 }
                 else {
-                    return "something went wrong";
+                    return{
+                        message: "something went wrong",
+                    };
                 }
 
         }
 
-    console.log(computerSelection, playerSelection)
-    console.log(playRound(computerSelection, playerSelection))
-    console.log(playerScore)
-    console.log(computerScore)
-    console.log(i)
+
+        console.log(computerSelection, playerSelection)
+        console.log(playRound())
+        console.log(playerScore)
+        console.log(computerScore)
+        console.log(i)
     }
 
     //announce the game winner 
     function announceResult(){
-        let pScore = playerScore;
-        let cScore = computerScore;
         if(i=rounds-1){
-            if(pScore > cScore){
+            if(playerScore > computerScore){
                 return "you smashed the computer and won"
             }
-            else if(cScore > pScore){
+            else if(computerScore > playerScore){
                 return "the computer smashed you and won"
             }
-            else if(cScore === pScore){
+            else if(computerScore === playerScore){
                 return "its a draw and noone won"
             }
         }
