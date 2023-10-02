a5fe6dd446d148091eaee9dac7f6b55a2ec7a65a
Tropfig <82678066+Tropfig@users.noreply.github.com>
2023-10-02 15:10:40 +0200

    added for loop 5 times and game function

--- gamescript.js
+++ gamescript.js
@@ -1,46 +1,52 @@
-const selection = [" rock"," paper"," scissors"];
-let playerChoice = prompt(`Choose between${selection} and beat the computer`,"")
-let computerSelection = computerChoice(selection);
-let playerSelection = playerChoice.toLowerCase();
-let playerScore = 0;
-let computerScore = 0;
+function game(){
+for(let i = 0; i<5; i++){
+    const selection = [" rock"," paper"," scissors"];
+    let playerChoice = prompt(`Choose between${selection} and beat the computer`,"")
+    let computerSelection = computerChoice(selection);
+    let playerSelection = playerChoice.toLowerCase();
+    let playerScore = 0;
+    let computerScore = 0;
 
-//this function pseudo random select a computer result
-function computerChoice(string) {
-    compChoice = Math.floor(Math.random() * string.length);
-    return string[compChoice];
-}
+    //this function pseudo random select a computer result
+    function computerChoice(string) {
+        compChoice = Math.floor(Math.random() * string.length);
+        return string[compChoice];
+        }
 
-//game function to select who beets who
-function playRound(computerSelection, playerSelection){
-    if (computerSelection.substr(1) === playerSelection){
-        return "draw"
+    //game function to select who beets who
+    function playRound(computerSelection, playerSelection){
+        if (computerSelection.substr(1) === playerSelection){
+            return "draw"
+        }
+            else if 
+            (computerSelection.substr(1) === "scissors" && playerSelection === "rock"){
+                return "you won, rock beats scissors";
+            }
+            else if (computerSelection.substr(1) === "rock" && playerSelection === "paper"){
+                return "you won, paper beats rock";
+            }
+            else if (computerSelection.substr(1) === "paper" && playerSelection === "scissors"){
+                return "you won, scissors beats paper";
+            }
+            else if (computerSelection.substr(1) === "rock" && playerSelection === "scissors"){
+                return "you lost, rock beats scissors";
+            }
+            else if (computerSelection.substr(1) === "paper" && playerSelection === "rock"){
+                return "you lost, paper beats rock";
+            }
+            else if (computerSelection.substr(1) === "scissors" && playerSelection === "paper"){
+                return "you lost, scissors beats paper";
+            }
+            else {
+                return "something went wrong";
+            }
+
+        }
+    console.log(playerSelection, computerSelection)
+    console.log(playRound(computerSelection, playerSelection))
+    console.log(playerScore)
+    console.log(computerScore)
     }
-        else if 
-        (computerSelection.substr(1) === "scissors" && playerSelection === "rock"){
-            return "you won, rock beats scissors";
-        }
-        else if (computerSelection.substr(1) === "rock" && playerSelection === "paper"){
-            return "you won, paper beats rock";
-        }
-        else if (computerSelection.substr(1) === "paper" && playerSelection === "scissors"){
-            return "you won, scissors beats paper";
-        }
-        else if (computerSelection.substr(1) === "rock" && playerSelection === "scissors"){
-            return "you lost, rock beats scissors";
-        }
-        else if (computerSelection.substr(1) === "paper" && playerSelection === "rock"){
-            return "you lost, paper beats rock";
-        }
-        else if (computerSelection.substr(1) === "scissors" && playerSelection === "paper"){
-            return "you lost, scissors beats paper";
-        }
-        else {
-            return "something went wrong";
-        }
-
 }
 
-console.log(computerSelection, playerSelection)
-console.log(playRound(computerSelection, playerSelection))
-console.log(computerScore, playerScore)
+game()
