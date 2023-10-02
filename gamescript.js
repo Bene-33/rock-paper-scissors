72a78aa5fb38db5302a4ffbf3e8893eed4fc1e92
Tropfig <82678066+Tropfig@users.noreply.github.com>
2023-10-02 14:41:11 +0200

    added player input choice in browser

--- gamescript.js
+++ gamescript.js
@@ -1,5 +1,5 @@
-const selection = ["rock","paper","scissors"];
-const playerChoice = "rock";
+const selection = [" rock"," paper"," scissors"];
+let playerChoice = prompt(`Choose between${selection} and beat the computer`,"")
 let computerSelection = computerChoice(selection);
 let playerSelection = playerChoice.toLowerCase();
 let playerScore = 0;
@@ -13,30 +13,31 @@ function computerChoice(string) {
 
 //game function to select who beets who
 function playRound(computerSelection, playerSelection){
-    if (computerSelection === playerSelection){
+    if (computerSelection.substr(1) === playerSelection){
         return "draw"
     }
-        else if (computerSelection === "scissors" && playerSelection === "rock"){
+        else if 
+        (computerSelection.substr(1) === "scissors" && playerSelection === "rock"){
             return "you won, rock beats scissors";
         }
-        else if (computerSelection === "rock" && playerSelection === "paper"){
+        else if (computerSelection.substr(1) === "rock" && playerSelection === "paper"){
             return "you won, paper beats rock";
         }
-        else if (computerSelection === "paper" && playerSelection === "scissors"){
+        else if (computerSelection.substr(1) === "paper" && playerSelection === "scissors"){
             return "you won, scissors beats paper";
         }
-        else if (computerSelection === "rock" && playerSelection === "scissors"){
+        else if (computerSelection.substr(1) === "rock" && playerSelection === "scissors"){
             return "you lost, rock beats scissors";
         }
-        else if (computerSelection === "paper" && playerSelection === "rock"){
+        else if (computerSelection.substr(1) === "paper" && playerSelection === "rock"){
             return "you lost, paper beats rock";
         }
-        else if (computerSelection === "scissors" && playerSelection === "paper"){
+        else if (computerSelection.substr(1) === "scissors" && playerSelection === "paper"){
             return "you lost, scissors beats paper";
         }
         else "error"
 }
 
-
 console.log(computerSelection, playerSelection)
 console.log(playRound(computerSelection, playerSelection))
+console.log(computerScore, playerScore)
