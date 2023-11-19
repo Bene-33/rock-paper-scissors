491db57185399fc52927e70db723fb47e6a2cb0f
Tropfig <82678066+Tropfig@users.noreply.github.com>
2023-11-19 14:38:42 +0100

    delete control js file, cant call the playRound function out of gamescript js

--- controls.js
+++
@@ -1,8 +0,0 @@
-let rock = document.querySelector('#button-rock');
-let paper = document.querySelector('#button-paper');
-let scissors = document.querySelector('#button-scissors');
-let button = document.querySelector('button');
-
-
-
-rock.addEventListener('onclick', playRound('rock'))
